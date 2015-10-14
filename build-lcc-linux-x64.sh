e6be2fcfc34ee6264d4fd052a6f4b60795aa6a2b
Timm Allman <tallman@cs.umass.edu>
2015-10-13 23:34:14 -0400

    fix build script

--- build-lcc-linux-x64.sh
+++ build-lcc-linux-x64.sh
@@ -3,13 +3,13 @@
 export BUILDDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/bld/x86/linux"
 mkdir -p $BUILDDIR
 ln -Tsfr ./include/x86/linux $BUILDDIR/include
-cp -Tr /usr/lib/gcc/*/* $BUILDDIR/gcc
+ln -Tsfr /usr/lib/gcc/*/* $BUILDDIR/gcc
 if [ ! -e $BUILDDIR/gcc/cpp ]; then
 	sudo ln -sf `which cpp` $BUILDDIR/gcc/cpp
 fi
 
 make clean
-make HOSTFILE=etc/linux.c CFLAGS='-DLCCDIR=\"$BUILDDIR\"' lcc
+make HOSTFILE=etc/linux.c CFLAGS='-DLCCDIR=\"$B\"' lcc
 make all
 # make TARGET=x86/linux test
 
--- lcc-json.sh
+++ lcc-json.sh
@@ -4,7 +4,7 @@ USAGE="$0 SOURCE_FILE OUTPUT_FILE"
 
 if [ "$1" != "" ]; then
 	if [ "$2" != "" ]; then
-		lcc -Wf-target=json -S $1 -o $2
+		bld/x86/linux/lcc -Wf-target=json -S $1 -o $2
 		exit
 	fi
 fi
